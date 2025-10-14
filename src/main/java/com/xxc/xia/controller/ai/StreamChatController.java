7f7d97f4c4f071498668eef70559128afdaf15bf
xxc <xxccode15@gmail.com>
2025-10-14 22:53:07 +0800

    ai test

--- src/main/java/com/xxc/xia/controller/ai/StreamChatController.java
+++ src/main/java/com/xxc/xia/controller/ai/StreamChatController.java
@@ -3,14 +3,20 @@ package com.xxc.xia.controller.ai;
 import com.alibaba.fastjson.JSONObject;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.openai.OpenAiChatModel;
 import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
 
 /**
  * desc..
@@ -25,22 +31,38 @@ public class StreamChatController {
     @Autowired
     private OpenAiChatModel baseChatModel;
     @Autowired
-    private OpenAiApi baseOpenAiApi;
-
+    private OpenAiApi       baseOpenAiApi;
+    @Autowired
+    private ChatMemory      chatMemory;
 
     @Operation(summary = "streamChat")
     @PostMapping("/chat")
     public Flux<String> chat(@RequestBody JSONObject req) {
-        Flux<String> output = ChatClient.builder(baseChatModel)
-                .build()
-                .prompt()
-                .user(req.getString("userMsg"))
-                .stream()
-                .content();
+        Flux<String> output = ChatClient.builder(baseChatModel).build().prompt()
+            .user(req.getString("userMsg")).stream().content();
         Flux<String> res = output.map(s -> {
             System.out.println(s);
             return s;
         });
+        return res;
+    }
+
+    @Operation(summary = "chatWithMemo")
+    @PostMapping("/chatWithMemo")
+    public Flux<String> chatWithMemo(@RequestParam("chatId") String chatId,
+                                     @RequestBody JSONObject req) {
+        Flux<String> output = ChatClient.builder(baseChatModel)
+            .defaultAdvisors(MessageChatMemoryAdvisor.builder(chatMemory).conversationId(chatId).build()).build().prompt()
+                .system("简介回答、格式美观即可")
+            .user(req.getString("userMsg")).stream().content();
+        Flux<String> res = output.map(s -> {
+            return s;
+        });
+        List<Message> messageList = chatMemory.get(chatId);
+        for (Message message : messageList) {
+            System.out.println(message);
+            System.out.println();
+        }
         return res;
     }
 }
